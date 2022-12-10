266c91018bb747479bfb283a947b6e8087cd7138
Lenon <lenoninformatica@gmail.com>
2022-12-09 23:42:58 -0300

    Iniciando

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  handleClick = () => {
+    const { history } = this.props;
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    /* Validando email */
+    const validationEmail = /\S+@\S+\.\S+/;
+    const emailOK = validationEmail.test(email);
+    /* validação senha maior que seis */
+    const SIX = 6;
+    const passwordOK = password.length >= SIX;
+    /* chamei ambos no estado de validação do botão */
+    const itsAvaliable = emailOK && passwordOK;
+    return (
+      <>
+        <h1>Trybe Wallet</h1>
+        <h2>Login</h2>
+        <input
+          id="emailCamp"
+          type="text"
+          required
+          data-testid="email-input"
+          placeholder="digite seu email"
+          name="email"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <input
+          id="senhaCamp"
+          type="password"
+          required
+          data-testid="password-input"
+          placeholder="digite sua senha"
+          name="password"
+          value={ password }
+          onChange={ this.handleChange }
+        />
+        <button
+          id="btn"
+          type="button"
+          onClick={ this.handleClick }
+          disabled={ !itsAvaliable }
+        >
+          Entrar
+
+        </button>
+      </>
+    );
   }
 }
+Login.propTypes = {
+  email: PropTypes.string,
+  password: PropTypes.string,
+  history: PropTypes.func,
+}.isRequired;
 
 export default Login;
