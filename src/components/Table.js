d8f15ef3289344a29015da0c3d5936860b9ef3b5
Lenon <lenoninformatica@gmail.com>
2022-12-18 19:35:14 -0300

    🚧 Requisito 6 Concluido

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,33 @@ import React, { Component } from 'react';
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <>
+        <h3>Table</h3>
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+          </tr>
+        </table>
+      </>
     );
   }
 }
